94a7088dba1aaa18d827617791b0e04b70741599
SrujanaVemula <47638293+SrujanaVemula@users.noreply.github.com>
2021-05-07 20:08:39 +0530

    Initial commit

---
+++ product_listing.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+
+<head>
+    <?php include("includes/include_css.html") ?>
+</head>
+
+<body>
+    <?php include("includes/browser_upgrade.html") ?>
+    <?php include("includes/sidebar.html") ?>
+
+    <main id="product_listing" class="smooth_scroll_pages product">
+        <!--  insert body content  -->
+        <section class="banner">
+            <picture>
+                <source media="(min-width:767px)" srcset="img/product/product_listing_banner.png">
+                <img src="img/product/product_listing_banner.png" alt="Banner Image" class="img-responsive">
+            </picture>
+            <div class="banner_content">
+                <h1 class="banner_title">Get your fitness fix today!</h1>
+            </div>
+        </section>
+        <section class="container">
+            <div class="breadcrumbs">
+                <p class="breadcrumbs_text"><span class="icon"><i class="fas fa-home"></i></span><a
+                        href="index.php">home / </a><a href="" class="current">product listing</a></p>
+            </div>
+        </section>
+        <section class="container">
+            <div class="filters_featured">
+                <div class="filters dropdown_wrap">
+                    <img src="img/product/filter.png" class="filter_img">
+                    <select name="state" class="browser-default uppercase dropdown" id="stateId">
+                        <option value="">show filters</option>
+                    </select>
+                </div>
+                <div class="featured dropdown_wrap">
+                    <img src="img/product/featured.png" class="filter_img">
+                    <span class="uppercase sort_text">sort: </span>
+                    <select name="state" class="browser-default uppercase dropdown" id="stateId">
+                        <option value="">featured</option>
+                    </select>
+                </div>
+            </div>
+        </section>
+        <section class="container">
+            <div class="products_list">
+                <div class="product_item">
+                    <img src="img/product/new_arrival_tag.png" class="img-responsive new_tag">
+                    <img src="img/product/product1.png" class="img-responsive product_img">
+                    <h2 class="product_title">Mass Gainer Whey Protein</h2>
+                    <div class="reviews">
+                        <div class="stars">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+                        <p class="review_text">16 reviews</p>
+                    </div>
+                    <div class="cost">
+                        <p class="strike">&#8377; 1500.00</p>
+                        <p class="current_price">&#8377; 1,044.00</p>
+                    </div>
+                </div>
+                <div class="product_item">
+                    <img src="img/product/product1.png" class="img-responsive product_img">
+                    <h2 class="product_title">Mass Gainer Whey Protein</h2>
+                    <div class="reviews">
+                        <div class="stars">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+                        <p class="review_text">16 reviews</p>
+                    </div>
+                    <div class="cost">
+                        <p class="strike">&#8377; 1500.00</p>
+                        <p class="current_price">&#8377; 1,044.00</p>
+                    </div>
+                </div>
+                <div class="product_item">
+                    <img src="img/product/new_arrival_tag.png" class="img-responsive new_tag">
+                    <img src="img/product/product1.png" class="img-responsive product_img">
+                    <h2 class="product_title">Mass Gainer Whey Protein</h2>
+                    <div class="reviews">
+                        <div class="stars">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+                        <p class="review_text">16 reviews</p>
+                    </div>
+                    <div class="cost">
+                        <p class="strike">&#8377; 1500.00</p>
+                        <p class="current_price">&#8377; 1,044.00</p>
+                    </div>
+                </div>
+                <div class="product_item">
+                    <img src="img/product/product1.png" class="img-responsive product_img">
+                    <h2 class="product_title">Mass Gainer Whey Protein</h2>
+                    <div class="reviews">
+                        <div class="stars">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+                        <p class="review_text">16 reviews</p>
+                    </div>
+                    <div class="cost">
+                        <p class="strike">&#8377; 1500.00</p>
+                        <p class="current_price">&#8377; 1,044.00</p>
+                    </div>
+                </div>
+                <div class="product_item">
+                    <img src="img/product/new_arrival_tag.png" class="img-responsive new_tag">
+                    <img src="img/product/product1.png" class="img-responsive product_img">
+                    <h2 class="product_title">Mass Gainer Whey Protein</h2>
+                    <div class="reviews">
+                        <div class="stars">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+                        <p class="review_text">16 reviews</p>
+                    </div>
+                    <div class="cost">
+                        <p class="strike">&#8377; 1500.00</p>
+                        <p class="current_price">&#8377; 1,044.00</p>
+                    </div>
+                </div>
+                <div class="product_item">
+                    <img src="img/product/product1.png" class="img-responsive product_img">
+                    <h2 class="product_title">Mass Gainer Whey Protein</h2>
+                    <div class="reviews">
+                        <div class="stars">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+                        <p class="review_text">16 reviews</p>
+                    </div>
+                    <div class="cost">
+                        <p class="strike">&#8377; 1500.00</p>
+                        <p class="current_price">&#8377; 1,044.00</p>
+                    </div>
+                </div>
+            </div>
+            <div class="btn_div">
+                <button class="green_btn">load more</button>
+            </div>
+        </section>
+        <section class="tab_section">
+            <div class="tabs"></div>
+        </section>
+        <section id="faq" class="faq">
+            <div class="text-center">
+                <h2 class="section_title">fAQ’S</h2>
+                <div class="container">
+                    <div class="main_accordian">
+                        <div class="accordian_item active">
+                            <h3 class="accordian_title header3">Amino Acids<span class="icon"><i class="fas fa-plus plus"></i><i
+                                        class="fas fa-minus minus"></i></h3>
+                            <div class="accordian_content">
+                                <p class="body1">The building blocks of muscle and tissue protein, amino acids are what promote muscle development and growth in the first place. There are 20 identified amino acids that make the thousands of proteins in our body. 9 out of these 20 are essential amino acids, meaning the body can’t make them and must be fulfilled through diet. 
+Amino acids play an even more crucial role when it comes to muscle development in body builders. When you work out, blood rushes to your muscles at a faster pace. Supplementing with essential amino acids pre and post work out increases protein synthesis in the muscles, aiding rapid development, catalysed by increased blood flow.<br>
+Advance MuscleMassTM manufactures a range of superior-quality essential and non-essential amino acid supplements at the most competitive price in the market. Not only do they enhance muscle recovery and strength but also improve your stamina and endurance, making them a must-have in every fitness buff’s kitty.  </p>
+                            </div>
+                        </div>
+                        <div class="accordian_item active">
+                        <h3 class="accordian_title header3">Pre Workout<span class="icon"><i class="fas fa-plus plus"></i><i
+                                        class="fas fa-minus minus"></i></h3>
+                            <div class="accordian_content">
+                                Content
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!--  end body content -->
+    <?php include("includes/include_js.html") ?>
+    <script>
+        $(".accordian_content").first().slideDown();
+        $(".accordian_title").click(function () {
+            $(this).parent().toggleClass("active");
+            $(this).parent().siblings().removeClass("active");
+            $(this).parent().find(".accordian_content").slideToggle();
+            $(this).parent().siblings().find(".accordian_content").slideUp();
+        })
+    </script>
+</body>
+
+</html>
